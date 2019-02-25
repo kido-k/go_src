9989395708043407a58bd070bafc3afa49e05096
kotaro_k <kotaro.k13@gmail.com>
2019-02-26 02:57:22 +0900

    separete folder handler and router

--- src/training/main.go
+++ src/training/main.go
@@ -3,33 +3,21 @@ package main
 import (
 	"fmt"
 	"net/http"
+
+	"github.com/julienschmidt/httprouter"
 )
 
-type HelloHandler struct{}
-
-func (h HelloHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "Hello!")
-}
-
-func log(h http.Handler) http.Handler {
-	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		fmt.Printf("Handler called - %T\n", h)
-		h.ServeHTTP(w, r)
-	})
-}
-
-func protect(h http.Handler) http.Handler {
-	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		h.ServeHTTP(w, r)
-	})
+func hello(w http.ResponseWriter, r *http.Request, p *httprouter.Params) {
+	fmt.Printf(w, "hello, %s\n", p.Byname("name"))
 }
 
 func main() {
-	server := http.Server{
-		Addr: "127.0.0.1:8080",
-	}
+	mux := httprouter.New()
+	mux.GET("/hello/:name", hello)
 
-	hello := HelloHandler{}
-	http.Handle("/hello", protect(log(hello)))
+	server := http.Server{
+		Addr:    "127.0.0.1:8080",
+		Handler: mux,
+	}
 	server.ListenAndServe()
 }
