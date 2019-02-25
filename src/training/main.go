0eac5da9736395076153b91628ca921323e790cc
kotaro_k <kotaro.k13@gmail.com>
2019-02-26 01:35:52 +0900

    add handler func

--- src/training/main.go
+++ src/training/main.go
@@ -5,17 +5,34 @@ import (
 	"net/http"
 )
 
-type MyHandler struct{}
+type Main struct{}
 
-func (h *MyHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+func (h *Main) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Main")
+}
+
+type HelloHandler struct{}
+
+func (h *HelloHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "Hello World")
 }
 
+type WorldHandler struct{}
+
+func (h *WorldHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "World!")
+}
+
 func main() {
-	handler := MyHandler{}
+	main := Main{}
+	hello := HelloHandler{}
+	world := WorldHandler{}
 	server := http.Server{
-		Addr:    "127.0.0.1:8080",
-		Handler: &handler,
+		Addr: "127.0.0.1:8080",
 	}
+
+	http.Handle("/", &main)
+	http.Handle("/hello", &hello)
+	http.Handle("/world", &world)
 	server.ListenAndServe()
 }
