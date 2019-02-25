00b758910c05ca2ccdf2ee0b75d6e909975b6fd3
kotaro_k <kotaro.k13@gmail.com>
2019-02-26 01:43:21 +0900

    update handle => handlerFunc

--- src/training/main.go
+++ src/training/main.go
@@ -5,34 +5,51 @@ import (
 	"net/http"
 )
 
-type Main struct{}
+// type Main struct{}
 
-func (h *Main) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+// func (h *Main) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+// 	fmt.Fprintf(w, "Main")
+// }
+
+// type HelloHandler struct{}
+
+// func (h *HelloHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+// 	fmt.Fprintf(w, "Hello World")
+// }
+
+// type WorldHandler struct{}
+
+// func (h *WorldHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+// 	fmt.Fprintf(w, "World!")
+// }
+
+func def(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "Main")
 }
 
-type HelloHandler struct{}
-
-func (h *HelloHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "Hello World")
+func hello(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Hello")
 }
 
-type WorldHandler struct{}
-
-func (h *WorldHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "World!")
+func world(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "World")
 }
 
 func main() {
-	main := Main{}
-	hello := HelloHandler{}
-	world := WorldHandler{}
+	// main := Main{}
+	// hello := HelloHandler{}
+	// world := WorldHandler{}
+
 	server := http.Server{
 		Addr: "127.0.0.1:8080",
 	}
 
-	http.Handle("/", &main)
-	http.Handle("/hello", &hello)
-	http.Handle("/world", &world)
+	// http.Handle("/", &main)
+	// http.Handle("/hello", &hello)
+	// http.Handle("/world", &world)
+
+	http.HandleFunc("/", def)
+	http.HandleFunc("/hello", hello)
+	http.HandleFunc("/world", world)
 	server.ListenAndServe()
 }
