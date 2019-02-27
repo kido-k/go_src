0fcafd881a31b4a8e059c690fdddf34ac6b86dc9
kotaro_k <kotaro.k13@gmail.com>
2019-02-27 23:40:34 +0900

    check range func in template engine

--- src/template/main.go
+++ src/template/main.go
@@ -13,11 +13,19 @@ func checkIfElse(w http.ResponseWriter, r *http.Request) {
 	t.Execute(w, rand.Intn(10) > 5)
 }
 
+func checkRange(w http.ResponseWriter, r *http.Request) {
+	t, _ := template.ParseFiles("range.html")
+	// daysOfWeek := []string{"月", "火", "水", "木", "金", "土", "日"}
+	daysOfWeek := []string{}
+	t.Execute(w, daysOfWeek)
+}
+
 func main() {
 	server := http.Server{
 		Addr: "127.0.0.1:8080",
 	}
 
 	http.HandleFunc("/ifelse", checkIfElse)
+	http.HandleFunc("/range", checkRange)
 	server.ListenAndServe()
 }
