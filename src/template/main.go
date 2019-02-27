75839af8931dacce296014343668bcab014a7315
kotaro_k <kotaro.k13@gmail.com>
2019-02-27 23:39:56 +0900

    check if_else func in template engine

---
+++ src/template/main.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"html/template"
+	"math/rand"
+	"net/http"
+	"time"
+)
+
+func checkIfElse(w http.ResponseWriter, r *http.Request) {
+	t, _ := template.ParseFiles("if_else.html")
+	rand.Seed(time.Now().Unix())
+	t.Execute(w, rand.Intn(10) > 5)
+}
+
+func main() {
+	server := http.Server{
+		Addr: "127.0.0.1:8080",
+	}
+
+	http.HandleFunc("/ifelse", checkIfElse)
+	server.ListenAndServe()
+}
