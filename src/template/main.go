d298e00f562880d96e10a2eec82fb36e6fc4de0f
kotaro_k <kotaro.k13@gmail.com>
2019-02-28 01:12:05 +0900

    add include func

--- src/template/main.go
+++ src/template/main.go
@@ -13,16 +13,21 @@ func checkIfElse(w http.ResponseWriter, r *http.Request) {
 	t.Execute(w, rand.Intn(10) > 5)
 }
 
-func checkWith(w http.ResponseWriter, r *http.Request) {
-	t, _ := template.ParseFiles("with.html")
-	t.Execute(w, "hello")
-}
-
 func checkRange(w http.ResponseWriter, r *http.Request) {
 	t, _ := template.ParseFiles("range.html")
 	// daysOfWeek := []string{"月", "火", "水", "木", "金", "土", "日"}
 	daysOfWeek := []string{}
 	t.Execute(w, daysOfWeek)
+}
+
+func checkWith(w http.ResponseWriter, r *http.Request) {
+	t, _ := template.ParseFiles("with.html")
+	t.Execute(w, "hello")
+}
+
+func checkInclude(w http.ResponseWriter, r *http.Request) {
+	t, _ := template.ParseFiles("t1.html", "t2.html")
+	t.Execute(w, "Hello World!")
 }
 
 func main() {
@@ -33,5 +38,6 @@ func main() {
 	http.HandleFunc("/ifelse", checkIfElse)
 	http.HandleFunc("/range", checkRange)
 	http.HandleFunc("/with", checkWith)
+	http.HandleFunc("/include", checkInclude)
 	server.ListenAndServe()
 }
