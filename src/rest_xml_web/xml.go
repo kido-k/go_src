268b72c4512d0d8289e828347cf818d8680e31a5
kotaro_k <kotaro.k13@gmail.com>
2019-03-01 17:06:37 +0900

    add xml decode func

--- src/rest_xml_web/xml.go
+++ src/rest_xml_web/xml.go
@@ -3,7 +3,7 @@ package main
 import (
 	"encoding/xml"
 	"fmt"
-	"io/ioutil"
+	"io"
 	"os"
 )
 
@@ -35,17 +35,40 @@ func main() {
 	}
 	defer xmlFile.Close()
 
-	xmlData, err := ioutil.ReadAll(xmlFile)
-	if err != nil {
-		fmt.Println("Error reading XML data:", err)
-		return
+	//xml decodeで要素毎の解析
+	decoder := xml.NewDecoder(xmlFile)
+	for {
+		t, err := decoder.Token()
+		if err == io.EOF {
+			break
+		}
+		if err != nil {
+			fmt.Println("Error decoding XML into tokens:", err)
+			return
+		}
+
+		switch se := t.(type) {
+		case xml.StartElement:
+			if se.Name.Local == "comment" {
+				var comment Comment
+				decoder.DecodeElement(&comment, &se)
+				fmt.Println("comment\n", comment)
+			}
+		}
 	}
 
-	var post Post
-	xml.Unmarshal(xmlData, &post)
-	// fmt.Println(post)
-	fmt.Println("Id\n", post.Id)
-	fmt.Println("Content\n", post.Content)
-	fmt.Println("Author\n", post.Author)
-	fmt.Println("Comments\n", post.Comments)
+	// //xml全文解析
+	// xmlData, err := ioutil.ReadAll(xmlFile)
+	// if err != nil {
+	// 	fmt.Println("Error reading XML data:", err)
+	// 	return
+	// }
+	// var post Post
+	// xml.Unmarshal(xmlData, &post)
+
+	// // fmt.Println(post)
+	// fmt.Println("Id\n", post.Id)
+	// fmt.Println("Content\n", post.Content)
+	// fmt.Println("Author\n", post.Author)
+	// fmt.Println("Comments\n", post.Comments)
 }
