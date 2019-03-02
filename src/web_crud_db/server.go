7c6af1a35b008837f2e45b469d5764846c2a228d
kotaro_k <kotaro.k13@gmail.com>
2019-03-02 20:53:01 +0900

    add crud db server

---
+++ src/web_crud_db/server.go
@@ -0,0 +1,101 @@
+package main
+
+import (
+	"encoding/json"
+	"net/http"
+	"path"
+	"strconv"
+)
+
+type Post struct {
+	Id      int    `json:"id"`
+	Content string `json:"content"`
+	Author  string `json:"author"`
+}
+
+func main() {
+	server := http.Server{
+		Addr: "127.0.0.1:8080",
+	}
+
+	http.HandleFunc("/post/", handleRequest)
+	server.ListenAndServe()
+}
+
+func handleRequest(w http.ResponseWriter, r *http.Request) {
+	var err error
+	switch r.Method {
+	case "GET":
+		err = handleGet(w, r)
+	case "POST":
+		err = handlePost(w, r)
+	case "PUT":
+		err = handlePut(w, r)
+	case "DELETE":
+		err = handleDelete(w, r)
+	}
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+}
+
+func handleGet(w http.ResponseWriter, r *http.Request) (err error) {
+	id, err := strconv.Atoi(path.Base(r.URL.Path))
+	checkError(err)
+
+	post, err := retrieve(id)
+	checkError(err)
+
+	output, err := json.MarshalIndent(&post, "", "\t\t")
+	checkError(err)
+
+	w.Header().Set("Content-Type", "application/json")
+	w.Write(output)
+	return
+}
+
+func handlePost(w http.ResponseWriter, r *http.Request) (err error) {
+	len := r.ContentLength
+	body := make([]byte, len)
+	r.Body.Read(body)
+	var post Post
+	json.Unmarshal(body, &post)
+	err = post.create()
+	checkError(err)
+
+	w.WriteHeader(200)
+	return
+}
+
+func handlePut(w http.ResponseWriter, r *http.Request) (err error) {
+	id, err := strconv.Atoi(path.Base(r.URL.Path))
+	checkError(err)
+
+	post, err := retrieve(id)
+	checkError(err)
+
+	len := r.ContentLength
+	body := make([]byte, len)
+	r.Body.Read(body)
+	json.Unmarshal(body, &post)
+	err = post.update()
+	checkError(err)
+
+	w.WriteHeader(200)
+	return
+}
+
+func handleDelete(w http.ResponseWriter, r *http.Request) (err error) {
+	id, err := strconv.Atoi(path.Base(r.URL.Path))
+	checkError(err)
+
+	post, err := retrieve(id)
+	checkError(err)
+
+	err = post.delete()
+	checkError(err)
+
+	w.WriteHeader(200)
+	return
+}
