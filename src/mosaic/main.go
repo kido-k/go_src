4f8cbb8408dfc977598bd27f57a9b4e059ca80f6
kotaro_k <kotaro.k13@gmail.com>
2019-03-06 23:47:23 +0900

    add main logic

--- src/mosaic/main.go
+++ src/mosaic/main.go
@@ -18,14 +18,15 @@ func main() {
 	mux := http.NewServeMux()
 	files := http.FileServer(http.Dir("public"))
 	mux.Handle("/static/", http.StripPrefix("/static/", files))
+
 	mux.HandleFunc("/", upload)
-	mux.HandleFunc("/", mosaic)
+	mux.HandleFunc("/mosaic", mosaic)
 	server := &http.Server{
 		Addr:    "127.0.0.1:8080",
 		Handler: mux,
 	}
+	TILESDB = tilesDB()
 
-	TILESDB := tilesDB()
 	fmt.Println("Mosaic server started.")
 	server.ListenAndServe()
 }
